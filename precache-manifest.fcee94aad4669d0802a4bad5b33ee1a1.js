97f95a9af4d4248d2bf2423fbb7b13979d1021a6
timdevcode <musictim@live.co.uk>
2018-12-07 08:47:53 +1030

    Updates

---
+++ precache-manifest.fcee94aad4669d0802a4bad5b33ee1a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2581e180eec6a465185f",
+    "url": "/robofriends-pwa/static/js/runtime~main.2581e180.js"
+  },
+  {
+    "revision": "c961dfb48e0f3a3ea3f4",
+    "url": "/robofriends-pwa/static/js/main.c961dfb4.chunk.js"
+  },
+  {
+    "revision": "99e1f6a58b298ee11876",
+    "url": "/robofriends-pwa/static/js/1.99e1f6a5.chunk.js"
+  },
+  {
+    "revision": "c961dfb48e0f3a3ea3f4",
+    "url": "/robofriends-pwa/static/css/main.07d4911f.chunk.css"
+  },
+  {
+    "revision": "99e1f6a58b298ee11876",
+    "url": "/robofriends-pwa/static/css/1.423dcddb.chunk.css"
+  },
+  {
+    "revision": "a250d26825ab53cdb1ded9b6f45606cd",
+    "url": "/robofriends-pwa/index.html"
+  }
+];
